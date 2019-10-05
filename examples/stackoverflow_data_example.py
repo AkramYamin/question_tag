65bdd9cf23154899daf07f6e06ec8af4bb0285d0
Akram Yamin <ak.5@yahoo.com>
2019-10-05 19:52:04 +0300

    train from local or clouded dataset

--- examples/stackoverflow_data_example.py
+++ examples/stackoverflow_data_example.py
@@ -1,4 +1,5 @@
 from src.processing.pre_processing import clean_text
 from src.modelling.train import train
 
-model = train(clean_text, "https://storage.googleapis.com/tensorflow-workshop-examples/stack-overflow-data.csv")
+# model = train(clean_text, "https://storage.googleapis.com/tensorflow-workshop-examples/stack-overflow-data.csv")
+model = train(clean_text, "../data/row/training_data.csv")
